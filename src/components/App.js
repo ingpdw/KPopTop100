b86f5640ac576d72c767310e12dc676132946f23
pdw <you@example.com>
2017-06-26 19:56:31 +0900

    auto

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,26 @@ import Header from './Header';
 import SongList from './SongList';
 import Search from './Search';
 import YoutubeFrame from './YoutubeFrame';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor( props ) {
-
     super( props );
     this.state = { query: '', target: '' };
+  }
+
+  componentDidMount() {
+    axios.post('/set', {
+        params: {
+          info: 'kpop'
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render(){
